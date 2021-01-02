b8ffe65250dd88fa981520ce595c14614d9b881f
MrAjulo <47714856+MrAjulo@users.noreply.github.com>
2021-01-02 13:34:08 +0100

    Add files via upload

---
+++ RPS.js
@@ -0,0 +1,65 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+        return userInput;
+    }
+    else {
+        return console.log('Error! Unknown factor.')
+    }
+    
+}
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber){
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'The Game is a Tie'
+    };
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'Computer won';
+        }
+        else {
+            return 'User won';
+        }
+    }
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'Computer won';
+        }
+        else {
+            return 'User won';
+        }
+    }
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'Computer won';
+        }
+        else {
+            return 'User won';
+        }
+    }
+}
+
+const playGame =  () => {
+    console.log(`player chose ${getUserChoice('scissors')}`);
+    console.log(`computer chose ${getComputerChoice()}`);
+    console.log(determineWinner(getUserChoice('scissors'), getComputerChoice()));
+}
+
+playGame();
+
+
